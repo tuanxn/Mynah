a6e385898c0d7885d5aee89ef3f64c38c82837ac
tuanxn <tuan.uci@gmail.com>
2020-07-03 07:30:12 -0700

    Add project files.

---
+++ Mynah/Automaton.cpp
@@ -0,0 +1,42 @@
+// Student ID: 20274909
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include "Automaton.h"
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+Automaton::Automaton(size_t num_parents, size_t rule) {
+
+}
+
+bool Automaton::set_rule(size_t rule) {
+
+}
+
+bool Automaton::equals(const Automaton& that) {
+
+}
+
+bool Automaton::make_next_gen(const vector<int>& current_gen, vector<int>& next_gen) {
+
+}
+
+string Automaton::get_first_n_generations(size_t n = 100, size_t width = 101) {
+
+}
+
+string Automaton::generation_to_string(const vector<int>& gen, size_t width) {
+
+}
+
+size_t Automaton::translate_n_bits_starting_at(const vector<int>& bits, size_t pos, size_t n) {
+
+}
